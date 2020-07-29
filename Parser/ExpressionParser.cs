5212a12747e41b3affa07865c6237501d82851e7
Suheyl Zafar <suheylz@hotmail.com>
2020-07-29 11:37:56 +0500

    fixed the issues in machines when a wrong input is recieved

---
+++ Parser/ExpressionParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Parser.Lexemes;
+
+namespace Parser
+{
+    public class ExpressionParser
+    {
+        readonly ILexemeProvider _provider;
+
+
+        public ExpressionParser(in string input)
+        {
+            _provider = new LexemeProvider(input);
+        }
+    }
+}
---
+++ Parser/Token.cs
@@ -0,0 +1,23 @@
+﻿namespace Parser
+{
+    public enum TokenType
+    {
+        Unknown = 0,
+        Identifier =1,
+        Operator =2,
+        String = 3,
+        Number = 4,
+        ChildExpressionBegins =5,
+        ChildExpressionEnds = 6
+    };
+
+
+    public readonly struct Token
+    {
+        public readonly TokenType type;
+        public readonly string value;
+
+        internal Token(TokenType type, string value) => (this.type, this.value) = (type, value);
+        public void Deconstruct(out TokenType type, out string value) => (type, value) = (this.type, this.value);
+    }
+}
